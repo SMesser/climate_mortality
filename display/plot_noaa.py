b538b32f3679d79229682dfe6b20631d4466d113
SMesser <SMesser@users.noreply.github.com>
2021-10-22 21:37:13 -0400

    Draft generation and plotting of interpolated NOAA data.

--- display/plot_noaa.py
+++ display/plot_noaa.py
@@ -7,6 +7,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 from os.path import join
+from scipy.interpolate import interp2d
 from yaml import safe_load
 
 with open('./files.yaml', 'r') as fp:
@@ -78,16 +79,99 @@ def plot_NOAA_var(var, year, month):
     ).show()
 
 
+def plot_interpolated(var, month, year):
+    '''Plot interpolated NOAA data.'''
+    df = interpolate_NOAA(var, year=year, month=month, kind='cubic')
+    fig = go.Figure(
+        data=go.Scattergeo(
+            lon=df['LONGITUDE'],
+            lat=df['LATITUDE'],
+            text=df[var],
+            mode='markers',
+            marker_color=df[var],
+            marker={
+                'colorscale': get_NOAA_colorscale(var),
+                'showscale': True
+            },
+        ),
+        layout={
+            'title': {'text': make_NOAA_title(var, year, month)}
+        }
+    ).show()
+    
+
 def plot_NOAA_samples():
     '''A collection of several NOAA datasets demonstrating data breadth.'''
-    '''
     plot_NOAA_var('PRCP', 1995, 10)
     plot_NOAA_var('EMNT', 2013, 7)
     plot_NOAA_var('TAVG', 1998, 8)
     plot_NOAA_var('TMIN', 2001, 11)
     plot_NOAA_var('TMAX', 1997, 9)
     plot_NOAA_var('EMXT', 1999, 4)
-    '''
     plot_NOAA_var('PRCP', 1995, 7)
     plot_NOAA_var('TAVG', 1995, 7)
     plot_NOAA_var('TAVG', 2015, 7)
+
+
+def _interpolate_HUMID(year, month, kind):
+    '''Generate a HUMIDITY proxy in mm-degrees Celsius.'''
+    tavg_df = load_NOAA(var='TAVG', month=month, year=year)
+    prcp_df = load_NOAA(var='PRCP', month=month, year=year)
+    interp_tavg = interp2d(
+        tavg_df['LONGITUDE'],
+        tavg_df['LATITUDE'],
+        tavg_df[var],
+        kind=kind,
+        copy=True,
+    )
+    interp_prcp = interp2d(
+        prcp_df['LONGITUDE'],
+        prcp_df['LATITUDE'],
+        prcp_df[var],
+        kind=kind,
+        copy=True,
+    )
+    return pd.DataFrame.from_dict([
+        {'LONGITUDE': x, 'LATITUDE': y, var: interp_tavg(x,y)*interp_prcp(x,y)}
+        for x in range(-175, 175)
+        for y in range(-60, 60)
+    ])
+
+
+def _interpolate_NOAA(var, year, month, kind):
+    '''Generate a map of NOAA data .'''
+    source_df = load_NOAA(var=var, month=month, year=year)
+    interpolated = interp2d(
+        source_df['LONGITUDE'],
+        source_df['LATITUDE'],
+        source_df[var],
+        kind=kind,
+        copy=True,
+    )
+    return pd.DataFrame.from_dict([
+        {'LONGITUDE': x, 'LATITUDE': y, var: interpolated(x,y)}
+        for x in range(-175, 175)
+        for y in range(-60, 60)
+    ])
+
+
+def interpolate_NOAA(var, year, month, kind='linear'):
+    '''Create 2D interpolated map across available observations.
+
+    <kind> is one of {'linear', 'cubic', 'quintic'}, according to the desired
+    order of the interpolating spline. The default is 'linear'.
+    
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.interp2d.html
+    '''
+    if var=='HUMID':
+        return _interpolate_HUMID(year, month, kind=kind)
+    else:
+        return _interpolate_NOAA(var, year, month, kind=kind)
+
+
+def plot_NOAA_interp():
+    plot_interpolated('TAVG', year=2015, month=7)
+    plot_interpolated('EMNT', year=2015, month=7)
+    plot_interpolated('EMXT', year=2015, month=7)
+    plot_interpolated('HUMID', year=2015, month=7)
+
--- show_data.py
+++ show_data.py
@@ -10,7 +10,7 @@ from os.path import join
 from yaml import safe_load
 
 from display.plot_cmip5 import plot_CMIP5_samples
-from display.plot_noaa import plot_NOAA_samples
+from display.plot_noaa import plot_NOAA_interp, plot_NOAA_samples
 from display.plot_who import (
     plot_WHO_samples, plot_WHO_raw_death_bar, plot_WHO_mortality_bar
 )
@@ -30,5 +30,5 @@ if __name__ == '__main__':
     # plot_WHO_samples()
     # plot_WHO_raw_death_bar(years=[2018])
     # plot_WHO_mortality_bar(years=[2018])
-    plot_CMIP5_samples()
-    
+    # plot_CMIP5_samples()
+    plot_NOAA_interp()
